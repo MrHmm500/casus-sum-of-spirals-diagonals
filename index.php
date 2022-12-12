2a365f801c79327328c477ea87e2e4ae67d1f557
ruby <ruby.van.soelen@omines.com>
2022-12-12 19:22:39 +0100

    Refactoring: small refactor on the style / logic

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ foreach($testCases as $caseName => $caseData) {
     echo calculateCross($spiral, $input) . '<br />';
 }
 
-function createSpiral($size): array {
+function createSpiral(int $size): array {
     $spiral = [];
     for ($x = 0; $x < $size; $x++) {
         $spiral[$x] = [];
@@ -33,70 +33,79 @@ function fillSpiral(array $spiral): array {
     $x = 0;
     $y = 0;
     $direction = 'r';
-    $i = 1;
+    $i = 2;
     $spiral[0][0] = 1;
 
-    // todo: refactor, dit is niet schaalbaar
     while ($filled == false) {
-        $i++;
         $nextDirection = null;
-        if ($direction == 'u') {
+
+        if ($direction === 'u') {
             $y--;
-            if(!isset($spiral[$y - 1][$x]) || $spiral[$y-1][$x] !== 0) {
+            if (!isset($spiral[$y - 1][$x]) || $spiral[$y - 1][$x] !== 0) {
                 $nextDirection = 'r';
             }
-            if ((isset($spiral[$y-1][$x]) === false || $spiral[$y-1][$x] !== 0) && !isset($spiral[$y][$x + 1]) || $spiral[$y][$x+1] !== 0) {
+            if ((isset($spiral[$y - 1][$x]) === false || $spiral[$y - 1][$x] !== 0) && isset($spiral[$y][$x + 1]) === false || $spiral[$y][$x+1] !== 0) {
                 $filled = true;
             }
         }
-        if ($direction == 'r') {
+
+        if ($direction === 'r') {
             $x++;
-            if(!isset($spiral[$y][$x + 1]) || $spiral[$y][$x+1] !== 0) {
+            if (!isset($spiral[$y][$x + 1]) || $spiral[$y][$x+1] !== 0) {
                 $nextDirection = 'd';
             }
-            if ((!isset($spiral[$y][$x + 1]) || $spiral[$y][$x+1] !== 0) && (!isset($spiral[$y + 1][$x]) || $spiral[$y+1][$x] !== 0)) {
+            if ((!isset($spiral[$y][$x + 1]) || $spiral[$y][$x+1] !== 0) && (isset($spiral[$y + 1][$x]) === false || $spiral[$y + 1][$x] !== 0)) {
                 $filled = true;
             }
         }
-        if ($direction == 'd') {
+
+        if ($direction === 'd') {
             $y++;
-            if(!isset($spiral[$y + 1][$x]) || $spiral[$y+1][$x] !== 0) {
+            if (!isset($spiral[$y + 1][$x]) || $spiral[$y + 1][$x] !== 0) {
                 $nextDirection = 'l';
             }
-            if ((isset($spiral[$y+1][$x]) === false || $spiral[$y+1][$x] !== 0) && !isset($spiral[$y][$x - 1]) || $spiral[$y][$x-1] !== 0) {
+            if ((isset($spiral[$y + 1][$x]) === false || $spiral[$y + 1][$x] !== 0) && isset($spiral[$y][$x - 1]) === false || $spiral[$y][$x-1] !== 0) {
                 $filled = true;
             }
         }
-        if ($direction == 'l') {
+
+        if ($direction === 'l') {
             $x--;
-            if(!isset($spiral[$y][$x - 1]) || $spiral[$y][$x-1] !== 0) {
+            if (!isset($spiral[$y][$x - 1]) || $spiral[$y][$x-1] !== 0) {
                 $nextDirection = 'u';
             }
-            if ((isset($spiral[$y][$x-1]) === false || $spiral[$y][$x-1] !== 0) && !isset($spiral[$y - 1][$x]) || $spiral[$y-1][$x] !== 0) {
+            if ((isset($spiral[$y][$x-1]) === false || $spiral[$y][$x-1] !== 0) && isset($spiral[$y - 1][$x]) === false || $spiral[$y - 1][$x] !== 0) {
                 $filled = true;
             }
         }
+
         $spiral[$y][$x] = $i;
-        if($nextDirection !== null) {
+
+        if ($nextDirection !== null) {
             $direction = $nextDirection;
         }
+
+        $i++;
     }
 
     return $spiral;
 }
 
-function calculateCross($spiral, $maxSize) {
+function calculateCross(array $spiral, int $maxSize): int {
     $alreadyAdded = [];
     $sum = 0;
-    for($x = 0; $x < $maxSize; $x++) {
+    $y = $maxSize -1;
+
+    for ($x = 0; $x < $maxSize; $x++) {
         $alreadyAdded[$x] = [];
         $alreadyAdded[$x][$x] = true;
         $sum += $spiral[$x][$x];
     }
-    $y = $maxSize -1;
-    for($x = 0; $x < $maxSize; $x++) {
+
+    for ($x = 0; $x < $maxSize; $x++) {
         $sum += isset($alreadyAdded[$x][$y]) ? 0 : $spiral[$x][$y];
         $y--;
     }
+
     return $sum;
 }
